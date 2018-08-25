6984cf7d32cc97ce0fb6b31626ec75936438066d
ncvanleeuwen <ncvanleeuwen@users.noreply.github.com>
2018-08-25 16:30:23 +0200

    disable automatic flash of firmware, use STVP GUI for flashing the resulting hex file

--- Start_Compiling.bat
+++ Start_Compiling.bat
@@ -4,7 +4,7 @@ del main.hex
 make -f Makefile_windows clean
 make -f Makefile_windows
 
-STVP_CmdLine -BoardName=ST-LINK -ProgMode=SWIM -Port=USB -Device=STM8S105x6 -FileProg=main.ihx -verbose -no_loop
+::STVP_CmdLine -BoardName=ST-LINK -ProgMode=SWIM -Port=USB -Device=STM8S105x6 -FileProg=main.ihx -verbose -no_loop
 
 pause
 exit

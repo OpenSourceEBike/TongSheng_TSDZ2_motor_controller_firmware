6593b15b6c575278518abd150a3fc06068bc5ef3
ncvanleeuwen <ncvanleeuwen@users.noreply.github.com>
2018-08-25 16:36:52 +0200

    makefile fixed for Windows

--- clean.bat
+++ clean.bat
@@ -1,13 +1,17 @@
+@ECHO OFF
+
 cd stdperiphlib\src
-del *.asm
-del *.rel
-del *.lk
-del *.lst
-del *.rst
-del *.sym
-del *.cdb
-del *.map
-del *.elf
-del *.bin
+del /q *.asm >NUL 2>NUL
+del /q *.rel >NUL 2>NUL
+del /q *.lk >NUL 2>NUL
+del /q *.lst >NUL 2>NUL
+del /q *.rst >NUL 2>NUL
+del /q *.sym >NUL 2>NUL
+del /q *.cdb >NUL 2>NUL
+del /q *.map >NUL 2>NUL
+del /q *.elf >NUL 2>NUL
+del /q *.bin >NUL 2>NUL
 cd..
 cd..
+
+@ECHO ON

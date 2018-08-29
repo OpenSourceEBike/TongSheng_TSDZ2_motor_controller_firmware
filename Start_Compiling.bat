3fbcddc423a1b34fcd09874f7bed68674cfa5ba3
ncvanleeuwen <{ID}+{username}@users.noreply.github.com>
2018-08-29 09:50:28 +0200

    Improved Windows build scripts, reintroduced flash command to build script on users request

--- Start_Compiling.bat
+++ Start_Compiling.bat
@@ -3,7 +3,7 @@ PATH = %PATH%;C:\Program Files (x86)\STMicroelectronics\st_toolset\stvp;C:\SDCC\
 make -f Makefile_windows clean
 make -f Makefile_windows
 
-::STVP_CmdLine -BoardName=ST-LINK -ProgMode=SWIM -Port=USB -Device=STM8S105x6 -FileProg=main.ihx -verbose -no_loop
+STVP_CmdLine -BoardName=ST-LINK -ProgMode=SWIM -Port=USB -Device=STM8S105x6 -FileProg=main.ihx -verbose -no_loop
 
 pause
 exit
